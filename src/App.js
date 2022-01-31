df3efb2a5091bfa519ef81d89cb95b0fc2052c20
Guada Iturralde <guada.iturralde@gmail.com>
2022-01-31 16:43:40 -0300

    feat: add tab functionality

--- src/App.js
+++ src/App.js
@@ -5,8 +5,14 @@ import NewsCard from "./components/news-card/news-card";
 
 const App = () => {
     const [selectedFilter, setSelectedFilter] = useState(null);
+    const [activeTab, setActiveTab] = useState(1);
 
     const handleSelect = (opt) => setSelectedFilter(opt);
+
+    const handleTabClick = (tab) => {
+        if (tab === activeTab) return;
+        setActiveTab(tab);
+    };
 
     return (
         <div className="app">
@@ -16,12 +22,28 @@ const App = () => {
 
             <main className="app__main">
                 <div className="app__tabs-container">
-                    <p className="app__tab app__tab-left app__tab--active">
+                    <p
+                        className={
+                            activeTab === 1
+                                ? "app__tab app__tab-left app__tab--active"
+                                : "app__tab app__tab-left"
+                        }
+                        onClick={() => handleTabClick(1)}
+                    >
                         All
                     </p>
-                    <p className="app__tab app__tab-right">My faves</p>
+                    <p
+                        className={
+                            activeTab === 2
+                                ? "app__tab app__tab-right app__tab--active"
+                                : "app__tab app__tab-right"
+                        }
+                        onClick={() => handleTabClick(2)}
+                    >
+                        My faves
+                    </p>
                 </div>
-                
+
                 <FilterSelect filter={selectedFilter} onselect={handleSelect} />
 
                 <section className="cards-container">
@@ -33,6 +55,6 @@ const App = () => {
             </main>
         </div>
     );
-}
+};
 
 export default App;
